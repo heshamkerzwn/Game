99a58f6a3ae3120608fa00b654e422f94e9e3347
heshamkerzwn <heshamkarzwn@gmail.com>
2023-11-26 16:51:30 +0300

    fixed 5th square

--- test.js
+++ test.js
@@ -18,7 +18,7 @@ function winner() {
     if (squares[1] == squares[2] && squares[2] == squares[3] && squares[1] != '') {
         end(1, 2, 3)
     }
-    else if (squares[4] == squares[5] && squares[5] == squares[6] && squares1[5] != '') {
+    else if (squares[4] == squares[5] && squares[5] == squares[6] && squares[5] != '') {
         end(4, 5, 6)
     }
     else if (squares[7] == squares[8] && squares[8] == squares[9] && squares[8] != '') {
